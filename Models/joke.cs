e0697b4adb739fc697564e331fcf8bc094956ebe
pacharya11 <94557347+pacharya11@users.noreply.github.com>
2021-12-18 17:14:35 -0600

    Add project files.

---
+++ Models/joke.cs
@@ -0,0 +1,28 @@
+﻿namespace WebApplication2.Models
+{
+    public class joke
+    {
+
+
+
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public joke()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
